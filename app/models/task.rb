2b4f1933d20ffbf9a78dee6f3e14c272faa26b64
martinushka <martinushka536@yandex.ru>
2023-09-28 18:45:01 +0300

    Association and validations(2)

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,7 +6,4 @@ class Task < ApplicationRecord
     validates :description, presence: true
     validates :author, presence: true
     validates :description, length: { maximum: 500 }
-    validates :first_name, presence: true, length: { minimum: 2 }
-    validates :last_name, presence: true, length: { minimum: 2 }
-    validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
 end
