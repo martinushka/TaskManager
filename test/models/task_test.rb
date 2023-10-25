e1a46cd273cf12720237b70331fc77cc78f07df5
martinushka <martinushka536@yandex.ru>
2023-10-25 21:08:40 +0300

    Website layout

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
   test 'create' do
-    task = create(:task)
+    task = create(:task, state: 'new_task')
     assert task.persisted?
   end
 end
