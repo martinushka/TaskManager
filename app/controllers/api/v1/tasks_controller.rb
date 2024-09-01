0cdfafc5f1fc0aa71106bbbb120743df52bd8d0c
Martin <martinushka536@yandex.ru>
2024-09-01 10:48:46 +0300

    API

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,52 @@
+class Api::V1::TasksController < Api::V1::ApplicationController
+  before_action :set_task, only: [:show, :update, :destroy]
+
+  def index
+    tasks = Task.all
+                .ransack(ransack_params)
+                .result
+                .page(page)
+                .per(per_page)
+
+    respond_with(tasks, each_serializer: TaskSerializer, root: 'items', meta: build_meta(tasks))
+  end
+
+   def show
+     task = Task.find(params[:id])
+
+     respond_with(task, serializer: TaskSerializer)
+   end
+
+  def create
+    task = current_user.my_tasks.new(task_params)
+    task.save
+
+    respond_with(task, serializer: TaskSerializer, location: nil)
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+
+    respond_with(task, serializer: TaskSerializer)
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+
+    respond_with(task)
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Task not found' }, status: :not_found
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :description, :author_id, :assignee_id, :state_event)
+  end
+end
